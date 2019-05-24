a3cd4ebc7874a9a2d58057e541bbe32ca05b05e8
Mobassir <mobassirhossen94@gmail.com>
2019-05-24 21:40:37 +0600

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 def home():
 	return render_template('home.html')
 
-@app.route('/resturantnlp.herokuapp.com/predict',methods=['POST'])
+@app.route('https://resturantnlp.herokuapp.com/predict',methods=['POST'])
 def predict():
     # Importing the dataset
     dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t', quoting=3)
