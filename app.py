38ad1c70ff7604045451aa1af3bc62ba1aa6ba45
Mobassir <mobassirhossen94@gmail.com>
2019-05-24 22:02:16 +0600

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ Created on Sat Aug 18 04:39:33 2018
 """
 from flask import Flask, render_template, url_for, request
 import pandas as pd
-import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.externals import joblib
